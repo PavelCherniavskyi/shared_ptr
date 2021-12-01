67e676db4d14b12e0fc09991029f3aaa25bc63c2
Pavel Cherniavskyi <cherniavskyi@gmail.com>
2021-12-01 20:28:43 +0200

    removed unnesessary cpp files

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "src/shared_ptr.hpp"
-
-int main(int argc, char ** argv)
-{
-  int* ptr = new int{5};
-  shared_ptr<int> testPtr(ptr);
-
-  delete ptr;
-}
--- tests/test_cases.cpp
+++ tests/test_cases.cpp
@@ -1,5 +1,11 @@
 #include <gtest/gtest.h>
 
+int main(int argc, char **argv) 
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
 TEST(SharedPtr, SharedPtrTest)
 {
   const int a = 5;
