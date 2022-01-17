20c3c58cecfdb0ec9103d35b044d51180585c8d1
Pavel Cherniavskyi <cherniavskyi@gmail.com>
2022-01-17 22:44:10 +0200

    Basic implementation + tests

--- src/shared_ptr.hpp
+++ src/shared_ptr.hpp
@@ -1,8 +1,109 @@
-# pragma once
+#pragma once
+
+#include <functional>
 
 template<typename T>
 class shared_ptr
 {
 public:
-  explicit shared_ptr(T*){}
+  shared_ptr()
+    : mValue(nullptr)
+    , mCtrlBlock(nullptr)
+  {
+  }
+
+  explicit shared_ptr(T* aPtr)
+    : mCtrlBlock(new control_block(1, 0))
+  {
+    mValue = aPtr;
+  }
+
+  explicit shared_ptr(T* aPtr, std::function<void(T*)> aDeleter)
+    : mCtrlBlock(new control_block(1, 0, aDeleter))
+  {
+    mValue = aPtr;
+  }
+
+  shared_ptr(const shared_ptr& aPtr) noexcept
+  {
+    if (aPtr.mCtrlBlock)
+    {
+      aPtr.mCtrlBlock->StrongRef++;
+    }
+
+    mValue = aPtr.mValue;
+    mCtrlBlock = aPtr.mCtrlBlock;
+  }
+
+  shared_ptr& operator=(const shared_ptr<T>& aPtr) noexcept
+  {
+    if (mCtrlBlock == aPtr.mCtrlBlock && mValue == aPtr.mValue)
+    {
+      return *this;
+    }
+
+    // assuming if mValue != nullptr means that mCtrlBlock can't be nullptr as well
+    if (mValue)
+    {
+      if (mCtrlBlock->StrongRef <= 1)
+      {
+        mCtrlBlock->Deleter ? mCtrlBlock->Deleter(mValue) : delete mValue;
+      }
+      else
+      {
+        mCtrlBlock->StrongRef--;
+      }
+    }
+
+    aPtr.mCtrlBlock->StrongRef++;
+    mValue = aPtr.mValue;
+    mCtrlBlock = aPtr.mCtrlBlock;
+    return *this;
+  }
+
+  T& operator*() const noexcept { return *mValue; }
+  T* operator->() const noexcept { return mValue; }
+  explicit operator bool() const noexcept { return mValue ? true : false; }
+
+  ~shared_ptr()
+  {
+    if (mValue)
+    {
+      if (mCtrlBlock->StrongRef <= 1)
+      {
+        mCtrlBlock->Deleter ? mCtrlBlock->Deleter(mValue) : delete mValue;
+        delete mCtrlBlock;
+        mCtrlBlock = nullptr;
+        mValue = nullptr;
+      }
+      else
+      {
+        mCtrlBlock->StrongRef--;
+      }
+    }
+  }
+
+  int use_count() { return mCtrlBlock ? mCtrlBlock->StrongRef : 0; }
+
+private:
+  struct control_block
+  {
+    explicit control_block(int aStrong, int aWeek)
+      : control_block(aStrong, aWeek, nullptr)
+    {
+    }
+    explicit control_block(int aStrong, int aWeek, std::function<void(T*)> aDel)
+      : StrongRef(aStrong)
+      , WeekRef(aWeek)
+      , Deleter(aDel)
+    {
+    }
+    int StrongRef;
+    int WeekRef;
+    std::function<void(T*)> Deleter;
+  };
+
+private:
+  T* mValue;
+  control_block* mCtrlBlock;
 };
--- tests/test_cases.cpp
+++ tests/test_cases.cpp
@@ -1,15 +1,117 @@
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
-int main(int argc, char **argv) 
+#include "../src/shared_ptr.hpp"
+
+int main(int argc, char** argv)
 {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
 
-TEST(SharedPtr, SharedPtrTest)
-{
-  const int a = 5;
-  const int b = 5;
-  EXPECT_EQ(a, b);
+class DeleterMock
+{ 
+public:
+  MOCK_METHOD(void, Deleter, (int* val));
+};
 
+using ::testing::_;
+
+TEST(SharedPtr, SharedPtrConstructorsTest)
+{
+  // empty constructor
+  shared_ptr<int> emptyPtr;
+  EXPECT_FALSE(emptyPtr);
+
+  // pointer constructor
+  shared_ptr<int> oneParamPtr(new int(5));
+  EXPECT_TRUE(oneParamPtr);
+  EXPECT_EQ(*oneParamPtr, 5);
+  EXPECT_EQ(oneParamPtr.use_count(), 1);
+  
+  //constructor with deleter
+  {
+    DeleterMock deleteMock;
+    shared_ptr<int> withDeleter(new int(5), [&deleteMock](auto* aPtr){ deleteMock.Deleter(aPtr); });
+    EXPECT_TRUE(withDeleter);
+    EXPECT_EQ(*withDeleter, 5);
+    EXPECT_EQ(withDeleter.use_count(), 1);
+    EXPECT_CALL(deleteMock, Deleter(_)).Times(1);
+  }
+  
+  //copy constructor
+  shared_ptr<int> origin(new int(7));
+  shared_ptr<int> copy(origin);
+  EXPECT_EQ(*origin, 7);
+  EXPECT_EQ(*copy, 7);
+  EXPECT_EQ(origin.use_count(), 2);
+  EXPECT_EQ(copy.use_count(), 2);
+
+}
+
+TEST(SharedPtr, AssignOperator)
+{
+  shared_ptr<int> origin(new int(5));
+  shared_ptr<int> copy(origin);
+  copy = origin;
+  EXPECT_EQ(copy.use_count(), 2);
+  EXPECT_EQ(origin.use_count(), 2);
+  
+  shared_ptr<int> clone;
+  clone = copy;
+  EXPECT_EQ(copy.use_count(), 3);
+  EXPECT_EQ(origin.use_count(), 3);
+  EXPECT_EQ(clone.use_count(), 3);
+
+  DeleterMock deleteMock;
+  shared_ptr<int> cloneWithValue(new int(5), [&deleteMock](auto* aPtr){ deleteMock.Deleter(aPtr); });
+  EXPECT_CALL(deleteMock, Deleter(_)).Times(1);
+  cloneWithValue = copy;
+
+  EXPECT_EQ(copy.use_count(), 4);
+  EXPECT_EQ(origin.use_count(), 4);
+  EXPECT_EQ(clone.use_count(), 4);
+  EXPECT_EQ(cloneWithValue.use_count(), 4);
+
+  shared_ptr<int> anotherOrigin(new int(10));
+  origin = anotherOrigin;
+  EXPECT_EQ(origin.use_count(), 2);
+  EXPECT_EQ(anotherOrigin.use_count(), 2);
+  EXPECT_EQ(copy.use_count(), 3);
+  EXPECT_EQ(clone.use_count(), 3);
+  EXPECT_EQ(cloneWithValue.use_count(), 3);
+}
+
+struct DummyStruct
+{
+   DummyStruct(int val) : A(val) {}
+   int A;
+};
+
+TEST(SharedPtr, DereferenceOperators)
+{
+  shared_ptr<DummyStruct> ptr(new DummyStruct(5));
+  EXPECT_EQ(ptr->A, 5);
+  EXPECT_EQ((*ptr).A, 5);
+}
+
+TEST(SharedPtr, BoolOperator)
+{
+  shared_ptr<int> hasValue(new int(5));
+  shared_ptr<int> empty;
+  EXPECT_TRUE(hasValue);
+  EXPECT_FALSE(empty);
+}
+
+TEST(SharedPtr, UseCount)
+{
+  shared_ptr<int> origin(new int(5));
+  EXPECT_EQ(origin.use_count(), 1);
+
+  {
+    shared_ptr<int> copy(origin);
+    EXPECT_EQ(copy.use_count(), 2);
+  }
+
+  EXPECT_EQ(origin.use_count(), 1);
 }
